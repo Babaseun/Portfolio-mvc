21e6e9657fa8fd67169ca992aebdc69b0f8aed97
Adeyemi Onibokun <adeyemi.onibokun@decagon.dev>
2020-12-23 12:54:02 +0100

    Added files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Skills()
+    {
+      return View();
+    }
+    public IActionResult Projects()
+    {
+      return View();
+    }
+  }
+}
